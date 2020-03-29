08ef7c75ff2a6f683255be556fbe92c1f9d744d5
dachi <ar.19920223@gmail.com>
2020-03-30 02:47:07 +0900

    書籍の一覧を取得するAPIを追加

--- main.go
+++ main.go
@@ -10,14 +10,62 @@ import (
 func main() {
 	e := echo.New()
 
+	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", hello)
+	e.GET("/books", list)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type Review struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Comment  string `json:"comment"`
+	Like     int    `json:"like"`
+}
+
+type Book struct {
+	ID       int      `json:"id"`
+	Title    string   `json:"title"`
+	Author   string   `json:"author"`
+	Overview string   `json:"overview"`
+	Image    string   `json:"image"`
+	Reviews  []Review `json:"reviews"`
+}
+
+func list(c echo.Context) error {
+	books := []Book{
+		{
+			ID:       1,
+			Title:    "はじめてのフロントエンド",
+			Author:   "執筆太郎",
+			Overview: "webフロントエンド開発初心者向けの一冊。JavaScriptの基礎から実際にアプリケーションを実装して動かすまでを完全サポート！",
+			Image:    "https://placehold.jp/148x210.png",
+			Reviews: []Review{
+				{
+					ID:       1,
+					Username: "開発初心者",
+					Comment:  "はじめてフロントエンド関連の書籍を買ったのですが、とても分かりやすく入門書として最高の一冊でした。最近プログラミングを始めた友達がいるのでこの本をオススメしておこうと思います。",
+					Like:     3,
+				},
+				{
+					ID:       1,
+					Username: "開発大好き",
+					Comment:  "会社のプログラミング研修用として購入しました。受講したメンバーからは分かりやすいととても好評でした。",
+					Like:     1,
+				},
+			},
+		},
+		{
+			ID:       2,
+			Title:    "現場で使えるフロントエンド",
+			Author:   "書籍花子",
+			Overview: "アプリケーションの実装だけではなく、テストやCI/CDといった現場の開発で必要になる知識を網羅！手元に置いておきたい一冊。",
+			Image:    "https://placehold.jp/148x210.png",
+			Reviews:  []Review{},
+		},
+	}
+	return c.JSON(http.StatusOK, books)
 }
