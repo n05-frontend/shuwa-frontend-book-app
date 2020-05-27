be4ff4388634d0263de24876b2cf5f6a0d6f1f1d
dachi <ar.19920223@gmail.com>
2020-05-28 01:13:15 +0900

    拡張子を .js から .ts へ変更

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/app.js',
+  entry: './src/app.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
