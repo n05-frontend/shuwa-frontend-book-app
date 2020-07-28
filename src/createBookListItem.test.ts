a9246de37d03064a9dc46df4290924fbd955753e
dachi <ar.19920223@gmail.com>
2020-07-28 23:43:51 +0900

    createBookListItem() の結果が DOM に反映できているかのテスト

---
+++ src/createBookListItem.test.ts
@@ -0,0 +1,32 @@
+import $ from 'jquery'
+import createBookListItem from './createBookListItem'
+import { Book } from './app'
+
+describe('createBookListItem()', () => {
+  const book: Book = {
+    id: 1,
+    title: 'フロントエンド開発',
+    author: '執筆太郎',
+    overview: 'フロントエンド開発をこれから始める方に最適な1冊です',
+    image: 'https://example.com/image.png',
+    reviews: [
+      {
+        id: 1,
+        username: '匿名ユーザー1',
+        comment: 'スムーズに開発することができました！',
+        like: 12
+      },
+      {
+        id: 2,
+        username: '匿名ユーザー2',
+        comment: '楽しく読ませてもらいました。',
+        like: 0
+      }
+    ]
+  }
+
+  test('should return DOM element', () => {
+    document.body.innerHTML = `<ul>${createBookListItem(book)}</ul>`
+    expect($('.book-list__item').length).toBe(1)
+  })
+})
