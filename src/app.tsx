ada71fcfcbc4555a8e7f32d1548084508b677260
dachi <ar.19920223@gmail.com>
2020-08-09 01:57:54 +0900

    移行前のコードを削除

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Timeline from './Timeline'
 
-import $ from 'jquery'
-import createBookListItem from './createBookListItem'
-import createBookReview from './createBookReview'
-
 export type Book = {
   id: number
   title: string
@@ -24,40 +20,3 @@ export type Review = {
 
 const root = document.getElementById('react-root')
 ReactDOM.render(<Timeline />, root)
-
-$(function() {
-  $.ajax('http://localhost:1323/books')
-    .done(function(books: Book[]) {
-      books.forEach(book => $('#js-book-list').append($(createBookListItem(book))))
-
-      $('.js-toggle-review').on('click', function(event) {
-        var bookId = $(this).data('bookId')
-        $('.js-review[data-book-id="' + bookId + '"]').toggle('fast')
-
-        return false
-      })
-
-      $(document).on('click', '.js-like', function(event) {
-        var likeCountElement = $(this).find('.js-like-count')
-        likeCountElement.text(likeCountElement.text() + 1)
-
-        return false
-      })
-
-      $(document).on('submit', '.js-form', function(event) {
-        var bookId = $(this).data('bookId')
-        $.ajax({
-          url: 'http://localhost:1323/reviews',
-          type: 'post',
-          dataType: 'json',
-          data: {
-            comment: $(event.currentTarget).find('textarea').val()
-          }
-        }).done(function(review) {
-          $('.js-review[data-book-id="' + bookId + '"] > ul').append($(createBookReview(review)))
-        })
-
-        return false
-      })
-    })
-})
