38ea8eecad8144646d21cd730ddda6c9bb6b1c5a
dachi <ar.19920223@gmail.com>
2020-08-09 00:53:14 +0900

    いいね数のカウントアップを実装

--- src/ReviewList.tsx
+++ src/ReviewList.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { Review } from './app'
+
+function ReviewListItem({ review }: { review: Review }) {
+  const [likeCount, setLikeCount] = useState(review.like)
+
+  return (
+    <li className="review__list__item">
+      <p className="review__list__item__name">{review.username}さんの感想・評価</p>
+      <p className="review__list__item__comment">{review.comment}</p>
+      <p className="review__list__item__like">
+        <a href="#" className="review__list__item__like__button" onClick={() => setLikeCount(likeCount + 1)}>
+          &#x2764;️ {likeCount}件
+        </a>
+      </p>
+    </li>
+  )
+}
 
 export default function ReviewList({ reviews }: { reviews: Review[] }) {
   return (
     <ul className="review__list">
-      {reviews.map(review => (
-        <li key={review.id} className="review_list__item">
-          <p className="review__list__item__name">{review.username}さんの感想・評価</p>
-          <p className="review__list__item__comment">{review.comment}</p>
-          <p className="review__list__item__like">
-            <a className="review__list__item__like__button">
-              &#x2764;️ {review.like}件
-            </a>
-          </p>
-        </li>
-      ))}
+      {reviews.map(review => <ReviewListItem key={review.id} review={review} />)}
     </ul>
   )
 }
