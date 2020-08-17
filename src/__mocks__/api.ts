ec33c400723a87a372737e06abecdf83bfb85923
dachi <ar.19920223@gmail.com>
2020-08-18 04:20:54 +0900

    api.ts のモックファイルを作成

---
+++ src/__mocks__/api.ts
@@ -0,0 +1,24 @@
+import type { Book } from '../app'
+
+export const dummyBooks: Book[] = [
+  {
+    id: 1,
+    title: 'フロントエンド開発',
+    author: '執筆太郎',
+    overview: 'フロントエンド開発をこれから始める方に最適な1冊です',
+    image: 'https://example.com/front-end.png',
+    reviews: [],
+  },
+  {
+    id: 2,
+    title: 'バックエンド開発',
+    author: '執筆太郎',
+    overview: 'バックエンド開発をこれから始める方に最適な1冊です',
+    image: 'https://example.com/back-end.png',
+    reviews: []
+  }
+]
+
+export async function getBooks(): Promise<Book[]> {
+  return dummyBooks
+}
