c2adcd09a891450fdbbcef5d1943472ecaded087
dachi <ar.19920223@gmail.com>
2020-08-18 05:23:52 +0900

    ページタイトルの表示を確認するテスト

---
+++ src/Timeline.test.tsx
@@ -0,0 +1,16 @@
+jest.mock('./api')
+
+import React from 'react'
+import { act } from 'react-dom/test-utils'
+import { mount } from 'enzyme'
+import Timeline from './Timeline'
+
+describe('<Timeline>', () => {
+
+  test('should return <h2> element', async () => {
+    await act(async () => {
+      const wrapper = mount(<Timeline />)
+      expect(wrapper.find('.page__title').text()).toBe('タイムライン')
+    })
+  })
+})
