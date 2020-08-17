a33d661af431afa0f88f6dcbede02f448b97419a
dachi <ar.19920223@gmail.com>
2020-08-18 00:25:57 +0900

    Babel の設定を切り出す

--- webpack.config.js
+++ webpack.config.js
@@ -17,17 +17,7 @@ module.exports = {
         test: /\.(j|t)sx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript'
-            ],
-            plugins: [
-              '@babel/plugin-transform-runtime'
-            ]
-          }
+          loader: 'babel-loader'
         }
       }
     ]
