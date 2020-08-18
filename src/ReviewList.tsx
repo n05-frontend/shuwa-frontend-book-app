119ae57faaf14261c0b46b0df638eee37ca70394
dachi <ar.19920223@gmail.com>
2020-08-19 01:45:50 +0900

    <ReviewList> のスタイル適用を styled-components で行う

--- src/ReviewList.tsx
+++ src/ReviewList.tsx
@@ -1,26 +1,62 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import type { Review } from './app'
+
+const Item = styled.li`
+  border-bottom: 1px solid #d6d6d6;
+  font-size: 1rem;
+  padding: 15px 0;
+`
+
+const Name = styled.p`
+  margin: 0;
+`
+
+const Comment = styled.p`
+  margin: 5px 0 0;
+`
+
+const Like = styled.p`
+  margin: 10px 0 0;
+  text-align: right;
+`
+
+const LikeButton = styled.a`
+  color: #000;
+  display: inline-block;
+  text-decoration: none;
+  &:hover {
+    color: #F43C3C;
+  }
+`
 
 function ReviewListItem({ review }: { review: Review }) {
   const [likeCount, setLikeCount] = useState(review.like)
 
   return (
-    <li className="review__list__item">
-      <p className="review__list__item__name">{review.username}さんの感想・評価</p>
-      <p className="review__list__item__comment">{review.comment}</p>
-      <p className="review__list__item__like">
-        <a href="#" className="review__list__item__like__button" onClick={() => setLikeCount(likeCount + 1)}>
+    <Item>
+      <Name>{review.username}さんの感想・評価</Name>
+      <Comment>{review.comment}</Comment>
+      <Like>
+        <LikeButton href="#" onClick={() => setLikeCount(likeCount + 1)}>
           &#x2764;️ {likeCount}件
-        </a>
-      </p>
-    </li>
+        </LikeButton>
+      </Like>
+    </Item>
   )
 }
 
+const List = styled.ul`
+  border-top: 1px solid #d6d6d6;
+  list-style: none;
+  margin-top: 20px;
+  padding: 0;
+`
+
 export default function ReviewList({ reviews }: { reviews: Review[] }) {
   return (
-    <ul className="review__list">
+    <List>
       {reviews.map(review => <ReviewListItem key={review.id} review={review} />)}
-    </ul>
+    </List>
   )
 }
