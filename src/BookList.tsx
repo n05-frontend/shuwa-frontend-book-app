154e7e35fc422371273d9314f0320d0b7ade664b
dachi <ar.19920223@gmail.com>
2020-08-20 23:49:51 +0900

    <BookList> のスタイル適用を styled-components で行う

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -1,8 +1,89 @@
 import React, { useState } from 'react'
 import { CSSTransition } from 'react-transition-group'
+import styled from 'styled-components'
 import ReviewList from './ReviewList'
 import { postReview } from './api'
 import type { Book } from './app'
+
+const Item = styled.li`
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.3);
+  margin-top: 30px;
+  padding: 30px;
+`
+
+const ItemInner = styled.div`
+  display: flex;
+`
+
+const Image = styled.img`
+  display: block;
+  flex-basis: 148px;
+  margin-right: 20px;
+`
+
+const Info = styled.div`
+  display: flex;
+  flex-basis: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Title = styled.h3`
+  font-size: 1.4rem;
+  margin: 0;
+`
+
+const Author = styled.span`
+  font-size: 1.1rem;
+`
+
+const Overview = styled.p`
+  color: #666;
+  font-size: 0.8rem;
+  margin: 0;
+`
+
+const Comment = styled.p`
+  font-size: 1rem;
+  margin: 0;
+  text-align: right;
+`
+
+const Link = styled.a`
+  color: #000;
+  text-decoration: underline;
+`
+
+const ReviewForm = styled.form`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 20px 0 0;
+`
+
+const ReviewInput = styled.textarea`
+  border-color: #d6d6d6;
+  border-radius: 3px;
+  box-sizing: border-box;
+  font-size: 1rem;
+  margin: 0;
+  padding: 8px;
+  width: 100%;
+`
+
+const ReviewSubmit = styled.button`
+  background-color: #0ae;
+  border: 0;
+  border-radius: 3px;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: bold;
+  margin-top: 10px;
+  padding: 10px 0;
+  width: 100%;
+`
 
 function BookListItem({ book }: { book: Book }) {
   const [showReview, setShowReview] = useState(false)
@@ -16,47 +97,51 @@ function BookListItem({ book }: { book: Book }) {
   }
 
   return (
-    <li key={book.id} className="book-list__item">
-      <div className="book-list__item__inner">
-        <img className="book-list__item__inner__image" src={book.image} alt={book.title} />
-        <div className="book-list__item__inner__info">
-          <h3 className="book-list__item__inner__info__title">
+    <Item>
+      <ItemInner>
+        <Image src={book.image} alt={book.title} />
+        <Info>
+          <Title>
             {book.title}
-            <span className="book-list__item__inner__info__title__author">
+            <Author>
               ({book.author})
-            </span>
-          </h3>
-          <p className="book-list__item__inner__info__overview">{book.overview}</p>
-          <p className="book-list__item__inner__info__comment">
-            <a href="#" className="book-list__item__inner__info__comment__link" onClick={() => { setShowReview(!showReview) }}>
+            </Author>
+          </Title>
+          <Overview>{book.overview}</Overview>
+          <Comment>
+            <Link href="#" onClick={() => { setShowReview(!showReview) }}>
               {reviews.length}件の感想・評価
-            </a>
-          </p>
-        </div>
-      </div>
+            </Link>
+          </Comment>
+        </Info>
+      </ItemInner>
       <CSSTransition in={showReview} timeout={200} classNames="review">
         <div className="review">
           <ReviewList reviews={reviews} />
-          <form className="review__form">
-            <textarea
-              className="review__form__input"
+          <ReviewForm>
+            <ReviewInput
               rows={5}
               placeholder={`「${book.title}」を読んだ感想・評価を教えてください`}
               onChange={(event) => setComment(event.currentTarget.value)}
               value={comment}
-            ></textarea>
-            <button className="review__form__submit" onClick={handleSubmit} type="button">投稿</button>
-          </form>
+            ></ReviewInput>
+            <ReviewSubmit onClick={handleSubmit} type="button">投稿</ReviewSubmit>
+          </ReviewForm>
         </div>
       </CSSTransition>
-    </li>
+    </Item>
   )
 }
 
+const List = styled.ul`
+  padding: 0;
+  list-style: none;
+`
+
 export default function BookList({ books }: { books: Book[] }) {
   return (
-    <ul className="book-list">
+    <List>
       {books.map(book => <BookListItem key={book.id} book={book} />)}
-    </ul>
+    </List>
   )
 }
